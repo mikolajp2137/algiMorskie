b20cf440635b9ac03ed3eb794d7e5883edb27f46
mikolajp2137 <73973110+mikolajp2137@users.noreply.github.com>
2022-11-29 13:16:40 +0100

    bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,3 +1,26 @@
-public class BubbleSort {
+import java.util.ArrayList;
 
+public class BubbleSort {
+    ArrayList<Integer> toBeSorted = new ArrayList<>();
+
+    public void sort(){
+        for(int i=0; i<toBeSorted.size()-1;i++){
+            for(int j=0;j<toBeSorted.size()-1;j++){
+                if(toBeSorted.get(j)>toBeSorted.get(j+1)){
+                    int tmp = toBeSorted.get(j);
+                    toBeSorted.set(j, toBeSorted.get(j + 1));
+                    toBeSorted.set(j+1,tmp);
+                }
+            }
+        }
+    }
+    public void printIt(){
+        for(int i=0; i<toBeSorted.size();i++){
+            System.out.print(toBeSorted.get(i)+" ");
+        }
+    }
+
+    public BubbleSort(ArrayList<Integer> someList){
+        toBeSorted = new ArrayList<Integer>(someList);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ArrayList<Integer> smallList = new ArrayList<>(50);
+        ArrayList<Integer> largeList = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 50; i++) {
+            smallList.add(i, rand.nextInt(10));
+        }
+        for (int i = 0; i < 50000; i++) {
+            largeList.add(i, rand.nextInt());
+        }
+        /*
+        for (int i = 0; i < 50; i++) {
+            System.out.print(largeList.get(i) + " ");
+        }*/
+
+        BubbleSort bubbleSmall = new BubbleSort(smallList);
+        BubbleSort bubbleLarge = new BubbleSort(largeList);
+        bubbleSmall.printIt();
+        long startTime = System.nanoTime();
+        bubbleSmall.sort();
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)/1000000;
+        System.out.println("\n");
+        bubbleSmall.printIt();
+        System.out.println("\nBubble sort execution time in milliseconds: "+duration);
+
+        long startTime2 = System.nanoTime();
+        bubbleLarge.sort();
+        long endTime2 = System.nanoTime();
+        long duration2 = (endTime2 - startTime2)/1000000;
+        System.out.println("\nBubble sort execution time in milliseconds for larger array: "+duration2);
     }
 }
